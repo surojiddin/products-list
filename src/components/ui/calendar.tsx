5c25a98661c98896eec6412d5efbaf704e773460
surojiddin <isurojiddin@gmail.com>
2025-11-27 15:16:26 +0500

    added weekStartsOn to DayPicker

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -26,6 +26,7 @@ function Calendar({
   return (
     <DayPicker
       showOutsideDays={showOutsideDays}
+      weekStartsOn={1}
       className={cn(
         "bg-background group/calendar p-3 [--cell-size:2rem] [[data-slot=card-content]_&]:bg-transparent [[data-slot=popover-content]_&]:bg-transparent",
         String.raw`rtl:**:[.rdp-button\_next>svg]:rotate-180`,
